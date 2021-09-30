08259cb15195b220afcbfa55cbe097ec6adaf14c
Denis BOURGE <bourge.denis@gmail.com>
2021-09-30 21:18:19 +0200

    feat: add namespace

--- MathExt.cs
+++ MathExt.cs
@@ -1,23 +1,30 @@
 using Godot;
 
-public static class MathExt {
-    public static Vector3 LerpVector3(Vector3 from, Vector3 to, float weight) {
-        return new Vector3(
-            Mathf.Lerp(from.x, to.x, weight),
-            Mathf.Lerp(from.y, to.y, weight),
-            Mathf.Lerp(from.z, to.z, weight)
-        );
-    }
+namespace SxGD
+{
+    public static class MathExt
+    {
+        public static Vector3 LerpVector3(Vector3 from, Vector3 to, float weight)
+        {
+            return new Vector3(
+                Mathf.Lerp(from.x, to.x, weight),
+                Mathf.Lerp(from.y, to.y, weight),
+                Mathf.Lerp(from.z, to.z, weight)
+            );
+        }
 
-    public static Transform AlignWithY(Transform transform, Vector3 newY) {
-        transform.basis.y = newY;
-        transform.basis.x = -transform.basis.z.Cross(newY);
-        transform.basis = transform.basis.Orthonormalized();
-        return transform;
-    }
+        public static Transform AlignWithY(Transform transform, Vector3 newY)
+        {
+            transform.basis.y = newY;
+            transform.basis.x = -transform.basis.z.Cross(newY);
+            transform.basis = transform.basis.Orthonormalized();
+            return transform;
+        }
 
-    public static Transform InterpolateAlignWithY(Transform transform, Vector3 newY, float weight) {
-        var aligned = AlignWithY(transform, newY);
-        return transform.InterpolateWith(aligned, weight);
+        public static Transform InterpolateAlignWithY(Transform transform, Vector3 newY, float weight)
+        {
+            var aligned = AlignWithY(transform, newY);
+            return transform.InterpolateWith(aligned, weight);
+        }
     }
 }
